083505bdf08dd80ae2af04de98b2cc53fc0d591e
Adel <142330462+Adel-Mtr@users.noreply.github.com>
2026-09-10 11:57:12 +0100

    Recognize Laravel's boolean parsing of the Render proxy flag

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->web(prepend: [PortfolioDemo::class]);
 
-        if (env('RENDER') === 'true') {
+        if (env('RENDER', false) === true) {
             $middleware->trustProxies(at: '*', headers: Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_PROTO);
         }
     })
